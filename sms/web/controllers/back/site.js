0b8f554f727a5c25dfd8c30ff7b39ca620adcac2
unknown <3203317@qq.com>
2015-08-11 18:37:07 +0800

    haha

--- sms/web/controllers/back/site.js
+++ sms/web/controllers/back/site.js
@@ -19,6 +19,7 @@ var fs = require('fs'),
 var macros = require('../../lib/macro');
 
 var biz = {
+	mobile: require('../../biz/mobile'),
 	send_plan: require('../../biz/send_plan'),
 	mobile_type: require('../../biz/mobile_type'),
 	zone: require('../../biz/zone')
@@ -228,232 +229,105 @@ function treeNode(docs){
 	return tree;
 }
 
-var sql = 'SELECT * FROM m_mobile WHERE id >= ((SELECT MAX(id) FROM m_mobile)-(SELECT MIN(id) FROM m_mobile)) * RAND() + (SELECT MIN(id) FROM m_mobile) LIMIT ?';
-
 var http = require('http');
 var iconv = require('iconv-lite');
 var parseString = require('xml2js').parseString;
 // var urlencode = require('urlencode');
 
-function getSQL(num){
-	return sql;
-}
-
-function getSQLNum(num){
-	var n = Math.ceil(num*0.1);
-	if(n>5000) n=5000;
-	return n
-}
-
+/**
+ *
+ * @params
+ * @return
+ */
 String.prototype.replaceAll = function(s1, s2) {  
-    var demo = this  
-    while (demo.indexOf(s1) != - 1)  
-    demo = demo.replace(s1, s2);  
-    return demo;  
+    var demo = this;
+    while(demo.indexOf(s1) != - 1)
+		demo = demo.replace(s1, s2);
+    return demo;
 }
 
-function getMobile(data, res){
-	var mobile = data.TestMobile.toString().replaceAll('\r\n', ',');
-	if(data.SmsNum <= 0){
-		// console.log(1)
-
-		startSend(data, res, mobile);
-
-	}else{
-		// console.log(2)
-		getMobilesFromDB(data, function (tels){
-			startSend(data, res, tels);
-		})
-	}
-	return mobile;
+/**
+ * 检测手机号
+ *
+ * @params
+ * @return
+ */
+function checkMobile(mobile){
+	var reg = /^0?1[3|4|5|8][0-9]\d{8}$/;
+	if(reg.test(mobile)) return mobile;
+	return '';
 }
 
-function getMobilesFromDB(data, cb){
-	pool.getConnection(function (err, conn) {
-		if(err) throw err;
-		conn.query(getSQL(data.SmsNum), [getSQLNum(data.SmsNum)], function (err, rows, fields){
-			if(err) throw err;
+/**
+ *
+ * @params
+ * @return
+ */
+exports.sendSMS = function(req, res, next){
+	var result = { success: false },
+		data = req._data;
 
-			var r = [];
+	if('' === data.Content.trim()) return next(new Error('短信内容不能为空，请填写！'));
 
-			// console.log('--------')
+	var user = req.session.user;
 
-			for (var i in rows) {
-				// console.log(rows[i].mobile);
-				r.push(rows[i].mobile)
+	// 获取发送计划
+	biz.send_plan.findFirstByUser(user.id, function (err, doc){
+		if(err) return next(err);
+		if(!doc) return next(new Error('今日您的预约发送量为0条，请联系客服！'));
+
+		data.Content = data.Content.substring(0, 70);
+
+		// 实际发送短信量
+		var n = Math.ceil(doc.PLAN_NUM * doc.RATIO);
+		n = 0;
+
+		// 随机发送号
+		biz.mobile.findRandom(n, function (err, docs){
+			if(err) return next(err);
+
+			var testMobile = data.TestMobile.toString().replaceAll('\r\n', ',').split(',');
+
+			for(var i in testMobile){
+				var mobile = checkMobile(testMobile[i]);
+				if('' !== mobile){
+					docs.push({ MOBILE: mobile });
+				}
 			}
 
-			conn.release();
+			console.log(docs);
 
-			cb(r.join(','));
+			result.data = data;
+			result.success = true;
+			res.send(result);
 		});
 	});
+};
+
+/**
+ *
+ * @params
+ * @return
+ */
+function startSend_1(content, mobiles, cb){
+	// TODO
 }
 
-function startSend(data, res, mobiles){
+/**
+ *
+ * @params
+ * @return
+ */
+function startSend_2(content, mobiles, cb){
 	var result = { success: false };
-
-	// console.log(mobiles);
-	// res.send(result);
-	// return;
 
 	var postData = require('querystring').stringify({
 		action: 'send',
 		userid: 766,
 		account: 'leiguang',
-		password: data.Password,
-		content: data.Content,
-		mobile: mobiles,
-		checkcontent: 0,
-		Channel: 5
-	});
-
-	var options = {
-		hostname: '114.215.196.225',
-		port: 9001,
-		path: '/sms.aspx?action=send',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': postData.length
-		}
-	};
-
-	var req = http.request(options, function (res1) {
-		// console.log('STATUS: '+ res.statusCode);
-		// console.log('HEADERS: '+ JSON.stringify(res.headers));
-		// res.setEncoding('utf8');
-		res1.on('data', function (chunk) {
-			result.code = 2;
-
-			parseString(chunk, function (err, resu) {
-				result.msg = resu;
-				res.send(result);
-			});
-		});
-	});
-
-	req.on('error', function (ex) {
-		console.log('problem with request: '+ ex.message);
-	});
-
-	// write data to request body
-	req.write(postData);
-	req.end();
-}
-
-exports.sendSMS = function(req, res, next){
-	var result = { success: false },
-		data = req._data;
-
-	var user = req.session.user;
-
-	biz.send_plan.findFirstByUser(user.id, function (err, doc){
-		if(err) return next(err);
-		if(!!doc) return next(new Error('今日您的预约发送量为0条，请联系客服！'));
-
-
-		data.Content = data.Content.substring(0, 70);
-
-		result.data = data;
-		result.success = true;
-		res.send(result);
-	});
-
-	// data = {
-	// 	Account: 'leiguang0371',
-	// 	Password: '818287',
-	// 	Content: '121',
-	// 	Phones: '13837186852,18530053050',
-	// 	Channel: '5'
-	// };
-
-	// var mobiles = getMobile(data, res);
-
-	// var postData = require('querystring').stringify({
-	// 	action: 'send',
-	// 	userid: 766,
-	// 	account: 'leiguang',
-	// 	password: data.Password,
-	// 	content: data.Content,
-	// 	mobile: mobiles,
-	// 	checkcontent: 0,
-	// 	Channel: 5
-	// });
-
-	// pool.getConnection(function (err, conn) {
-	// 	if(err) throw err;
-	// 	conn.query(sql, function (err, rows, fields){
-	// 		if(err) throw err;
-
-	// 		for (var i in rows) {
-	// 			console.log(rows[i]);
-	// 		}
-
-	// 		conn.release();
-
-	// 		/* http post */
-
-	// 		var options = {
-	// 			hostname: '114.215.196.225',
-	// 			port: 9001,
-	// 			path: '/sms.aspx?action=send',
-	// 			method: 'POST',
-	// 			headers: {
-	// 				'Content-Type': 'application/x-www-form-urlencoded',
-	// 				'Content-Length': postData.length
-	// 			}
-	// 		};
-
-	// 		var req = http.request(options, function (res1) {
-	// 			// console.log('STATUS: '+ res.statusCode);
-	// 			// console.log('HEADERS: '+ JSON.stringify(res.headers));
-	// 			// res.setEncoding('utf8');
-	// 			res1.on('data', function (chunk) {
-	// 				result.code = 2;
-
-	// 				parseString(chunk, function (err, resu) {
-	// 					result.msg = resu;
-	// 					res.send(result);
-	// 				});
-	// 			});
-	// 		});
-
-	// 		req.on('error', function (ex) {
-	// 			console.log('problem with request: '+ ex.message);
-	// 		});
-
-	// 		// write data to request body
-	// 		req.write(postData);
-	// 		req.end();
-	// 	});
-	// });
-};
-
-var mysql = require('mysql');
-
-var pool = mysql.createPool({
-	host: '127.0.0.1',
-	user: 'root',
-	password: 'password',
-	database: 'sms',
-	port: 22306
-});
-
-
-
-
-
-function startSend_1(content, cb){
-	var result = { success: false };
-
-	var postData = querystring.stringify({
-		action: 'send',
-		userid: 766,
-		account: 'leiguang',
-		password: 'password',
+		password: 'zs818287',
 		content: content,
-		mobile: mobiles,
+		mobile: mobiles.join(','),
 		checkcontent: 0,
 		Channel: 5
 	});
@@ -469,15 +343,16 @@ function startSend_1(content, cb){
 		}
 	};
 
-	var req = http.request(options, function (res1) {
+	var req = http.request(options, function (res) {
 		// console.log('STATUS: '+ res.statusCode);
 		// console.log('HEADERS: '+ JSON.stringify(res.headers));
 		// res.setEncoding('utf8');
-		res1.on('data', function (chunk) {
-			result.code = 2;
+		res.on('data', function (chunk) {
+			parseString(chunk, function (err, result) {
+				if(err) return cb(err);
 
-			parseString(chunk, function (err, resu) {
-				result.msg = resu;
+				result.msg = result;
+				result.success = true;
 				res.send(result);
 			});
 		});
